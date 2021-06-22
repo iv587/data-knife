0cdc49e3566a880daeeb874270ca52e850349a38
lijunfei <developerljf2020@163.com>
2021-06-22 10:26:37 +0800

    修改配置文件读取方式，使用viper

--- cfg/init.go
+++ cfg/init.go
@@ -1,81 +1,31 @@
 package cfg
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
-	"strings"
+	"github.com/spf13/viper"
 )
 
 const path = "conf"
-const confPath = path + string(os.PathSeparator) + "app.conf"
-
-const defaultConf = `server.addr=:8888
-log.enable=true
-log.path=logs
-`
 
 var configMap = make(map[string]string)
 
+var appConf = viper.New()
+
 func InitCfg() error {
-	_, err := os.Stat(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			os.MkdirAll(path, os.ModePerm)
-		} else {
-			return err
-		}
-	}
-	checkOrCreate(confPath, []byte(defaultConf))
-	byte, err := ioutil.ReadFile(confPath)
-	if err != nil {
-		return err
-	}
-	configStr := string(byte)
-	configKvs := strings.Split(configStr, "\n")
-	for _, i2 := range configKvs {
-		if i2 != "" {
-			configKv := strings.Split(i2, "=")
-			if len(configKv) > 1 {
-				configMap[configKv[0]] = configKv[1]
-			}
-		}
-	}
+	appConf.SetConfigName("app")
+	appConf.SetConfigType("yaml")
+	appConf.AddConfigPath(path)
+	appConf.SetDefault("server.addr", ":8080")
+	appConf.SetDefault("log.enable", true)
+	appConf.SetDefault("log.path", "logs")
+	appConf.SafeWriteConfig()
+	err := appConf.ReadInConfig()
 	return err
 }
 
-func checkOrCreate(path string, byte []byte) error {
-	f, err := os.Stat(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return ioutil.WriteFile(path, byte, os.ModePerm)
-		}
-		return err
-	}
-	if f.IsDir() {
-		return errors.New(fmt.Sprintf("初始化失败，勿在当前目录下存放名称为%s的目录", path))
-	}
-	return nil
-}
-
-func GetCfg(key string, defaultVal string) string {
-	val, ok := configMap[key]
-	if !ok {
-		return defaultVal
-	}
-	return val
+func GetCfg(key string) string {
+	return appConf.GetString(key)
 }
 
 func GetBoolCfg(key string) bool {
-	val, ok := configMap[key]
-	if ok {
-		bVal, err := strconv.ParseBool(val)
-		if err != nil {
-			return false
-		}
-		return bVal
-	}
-	return false
+	return appConf.GetBool(key)
 }
